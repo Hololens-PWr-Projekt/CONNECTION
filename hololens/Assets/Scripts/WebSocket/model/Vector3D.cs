9df43a16916212aad10f919dd14174289fa9189f
AdrakPro <adrak99.adrak99@gmail.com>
2024-11-18 20:32:10 +0100

    Added data models

---
+++ hololens/Assets/Scripts/WebSocket/model/Vector3D.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Model.Vector3D
+{
+    [Serializable]
+    public class Vector3D
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+}
