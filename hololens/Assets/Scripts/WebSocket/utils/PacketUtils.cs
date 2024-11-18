50d523ea57a7d5acadb98c5d4dfd42cd67045aff
AdrakPro <adrak99.adrak99@gmail.com>
2024-11-18 20:35:38 +0100

    Splitting packets helper

---
+++ hololens/Assets/Scripts/WebSocket/utils/PacketUtils.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System;
+using System.Text;
+using Model.Packet;
+using Manager.Json;
+
+namespace Utils.Packets
+{
+    public static class PacketUtils
+    {
+        private const int MAX_CHUNK_BYTES = 256;
+
+        public static List<Packet> SplitData(string packetId, PacketType type, string data)
+        {
+            List<Packet> packets = new();
+
+            string jsonData = JsonManager.ToJson(data);
+            // Implement in json manager
+            byte[] dataBytes = Encoding.UTF8.GetBytes(jsonData);
+            int totalPackets = (int)Math.Ceiling((double)dataBytes.Length / MAX_CHUNK_BYTES);
+            int totalSize = dataBytes.Length;
+
+            for (int i = 0; i < totalPackets; i++)
+            {
+                int start = i * MAX_CHUNK_BYTES;
+                int length = Math.Min(MAX_CHUNK_BYTES, totalSize - start);
+
+                // Extract the byte chunk
+                byte[] chunkBytes = new byte[length];
+                Array.Copy(dataBytes, start, chunkBytes, 0, length);
+
+                // Convert byte chunk back to string for packet
+                string chunk = Encoding.UTF8.GetString(chunkBytes);
+                bool isLast = i == totalPackets - 1;
+
+                packets.Add(new Packet(packetId, type, i, isLast, chunk));
+            }
+
+            return packets;
+        }
+    }
+}
