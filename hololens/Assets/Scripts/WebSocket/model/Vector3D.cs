713c476529cc7d22c11cc1dedbe8d315d8ac460c
AdrakPro <adrak99.adrak99@gmail.com>
2024-11-20 11:32:46 +0100

    Added constructor & getters & setters to Vector3D

--- hololens/Assets/Scripts/WebSocket/model/Vector3D.cs
+++ hololens/Assets/Scripts/WebSocket/model/Vector3D.cs
@@ -5,8 +5,19 @@ namespace Model.Vector3D
     [Serializable]
     public class Vector3D
     {
-        public float x;
-        public float y;
-        public float z;
+        private float x;
+        private float y;
+        private float z;
+
+        public Vector3D(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
+        public float Z { get => z; set => z = value; }
     }
 }
