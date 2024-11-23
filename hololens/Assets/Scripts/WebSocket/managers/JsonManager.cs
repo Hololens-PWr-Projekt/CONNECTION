4b59bfa9c0d01f1de4957b8ca29e98a2e6a74755
AdrakPro <adrak99.adrak99@gmail.com>
2024-11-23 16:32:49 +0100

    Added Unity support for lang v10

--- hololens/Assets/Scripts/WebSocket/managers/JsonManager.cs
+++ hololens/Assets/Scripts/WebSocket/managers/JsonManager.cs
@@ -5,73 +5,72 @@ using UnityEngine;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
-namespace Manager.Json
+namespace Manager.Json;
+
+public static class JsonManager
 {
-    public static class JsonManager
+    private static readonly string DataPath = Path.Combine(Application.dataPath, "Scripts", "data");
+
+    public static void SetSerializerSettings()
     {
-        private static readonly string DataPath = Path.Combine(Application.dataPath, "Scripts", "data");
-
-        public static void SetSerializerSettings()
+        JsonConvert.DefaultSettings = () => new JsonSerializerSettings
         {
-            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
-            };
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
 
-        }
-
-        // Load JSON and deserialize its content into a dictionary
-        public static Dictionary<string, object> LoadFromFile(string fileName)
-        {
-            string filePath = Path.Combine(DataPath, fileName + ".json");
-
-            try
-            {
-                if (!File.Exists(filePath))
-                {
-                    Debug.LogWarning($"JsonManager - File not found at path: {filePath}");
-                    return null;
-                }
-
-                string jsonContent = File.ReadAllText(filePath);
-                return Deserialize<Dictionary<string, object>>(jsonContent);
-            }
-            catch (Exception e)
-            {
-                Debug.LogError($"JsonManager - Failed to load JSON file: {e.Message}");
-                return null;
-            }
-        }
-
-        // Serialize data into JSON format, keeping original property names
-        public static string Serialize<T>(T data)
-        {
-            try
-            {
-                return JsonConvert.SerializeObject(data, Formatting.None);
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError($"JsonManager - Failed to serialize object. Exception: {ex.Message}");
-                return null;
-            }
-        }
-
-        // Deserialize JSON string into an object, respecting original property names
-        public static T Deserialize<T>(string jsonData)
-        {
-            try
-            {
-                return JsonConvert.DeserializeObject<T>(jsonData);
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError($"JsonManager - Failed to deserialize object. Exception: {ex.Message}");
-                return default;
-            }
-        }
-
-        // Helper function to get the file path
-        private static string GetFilePath(string fileName) => Path.Combine(DataPath, $"{fileName}.json");
     }
+
+    // Load JSON and deserialize its content into a dictionary
+    public static Dictionary<string, object> LoadFromFile(string fileName)
+    {
+        string filePath = Path.Combine(DataPath, fileName + ".json");
+
+        try
+        {
+            if (!File.Exists(filePath))
+            {
+                Debug.LogWarning($"JsonManager - File not found at path: {filePath}");
+                return null;
+            }
+
+            string jsonContent = File.ReadAllText(filePath);
+            return Deserialize<Dictionary<string, object>>(jsonContent);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"JsonManager - Failed to load JSON file: {e.Message}");
+            return null;
+        }
+    }
+
+    // Serialize data into JSON format, keeping original property names
+    public static string Serialize<T>(T data)
+    {
+        try
+        {
+            return JsonConvert.SerializeObject(data, Formatting.None);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"JsonManager - Failed to serialize object. Exception: {ex.Message}");
+            return null;
+        }
+    }
+
+    // Deserialize JSON string into an object, respecting original property names
+    public static T Deserialize<T>(string jsonData)
+    {
+        try
+        {
+            return JsonConvert.DeserializeObject<T>(jsonData);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"JsonManager - Failed to deserialize object. Exception: {ex.Message}");
+            return default;
+        }
+    }
+
+    // Helper function to get the file path
+    private static string GetFilePath(string fileName) => Path.Combine(DataPath, $"{fileName}.json");
 }
--- hololens/Assets/Scripts/WebSocket/managers/NetworkManager.cs
+++ hololens/Assets/Scripts/WebSocket/managers/NetworkManager.cs
@@ -4,108 +4,106 @@ using UnityEngine;
 using Manager.Json;
 using Model.Packet;
 
-namespace Manager.Networking
+namespace Manager.Networking;
+public class NetworkManager : MonoBehaviour
 {
-  public class NetworkManager : MonoBehaviour
+  public delegate void PacketSentCallback(Packet packet);
+  public delegate void AllPacketsSentCallback(string packetId);
+
+  private WebSocket webSocket;
+  private Dictionary<string, List<Packet>> pendingPackets;
+
+  void Awake()
   {
-    public delegate void PacketSentCallback(Packet packet);
-    public delegate void AllPacketsSentCallback(string packetId);
+    pendingPackets = new Dictionary<string, List<Packet>>();
+    ConnectToServer();
+  }
 
-    private WebSocket webSocket;
-    private Dictionary<string, List<Packet>> pendingPackets;
+  async void ConnectToServer()
+  {
+    webSocket = new WebSocket("ws://localhost:8080/ws/hololens");
 
-    void Awake()
+    webSocket.OnOpen += () => Debug.Log("NetworkManager - Connection open!");
+    webSocket.OnError += (e) => Debug.Log("NetworkManager - Error! " + e);
+    webSocket.OnClose += (e) => Debug.Log("NetworkManager - Connection closed!");
+
+    // Receive data from the server
+    webSocket.OnMessage += (bytes) =>
     {
-      pendingPackets = new Dictionary<string, List<Packet>>();
-      ConnectToServer();
+      var message = System.Text.Encoding.UTF8.GetString(bytes);
+      Debug.Log("Received OnMessage! (" + bytes.Length + " bytes) " + message);
+    };
+
+    await webSocket.Connect();
+  }
+
+  public async void SendPackets(List<Packet> packets, PacketSentCallback onPacketSent = null, AllPacketsSentCallback onAllPacketsSent = null)
+  {
+    if (ArePacketsEmpty(packets))
+    {
+      Debug.LogWarning("NetworkManager - No packed to send.");
+      return;
     }
 
-    async void ConnectToServer()
+    string packetId = packets[0].PacketId;
+
+    if (!pendingPackets.ContainsKey(packetId))
     {
-      webSocket = new WebSocket("ws://localhost:8080/ws/hololens");
-
-      webSocket.OnOpen += () => Debug.Log("NetworkManager - Connection open!");
-      webSocket.OnError += (e) => Debug.Log("NetworkManager - Error! " + e);
-      webSocket.OnClose += (e) => Debug.Log("NetworkManager - Connection closed!");
-
-      // Receive data from the server
-      webSocket.OnMessage += (bytes) =>
-      {
-        var message = System.Text.Encoding.UTF8.GetString(bytes);
-        Debug.Log("Received OnMessage! (" + bytes.Length + " bytes) " + message);
-      };
-
-      await webSocket.Connect();
+      pendingPackets[packetId] = new List<Packet>(packets);
     }
 
-    public async void SendPackets(List<Packet> packets, PacketSentCallback onPacketSent = null, AllPacketsSentCallback onAllPacketsSent = null)
+    foreach (var packet in packets)
     {
-      if (ArePacketsEmpty(packets))
-      {
-        Debug.LogWarning("NetworkManager - No packed to send.");
-        return;
-      }
+      string packetJson = JsonManager.Serialize(packet);
 
-      string packetId = packets[0].PacketId;
-
-      if (!pendingPackets.ContainsKey(packetId))
-      {
-        pendingPackets[packetId] = new List<Packet>(packets);
-      }
-
-      foreach (var packet in packets)
-      {
-        string packetJson = JsonManager.Serialize(packet);
-
-        if (IsWebSocketOpen())
-        {
-          await webSocket.SendText(packetJson);
-          onPacketSent?.Invoke(packet);
-          pendingPackets[packetId].Remove(packet);
-
-          Debug.Log($"Packet sent: {packet.PacketId}, index: {packet.Index}, isLast: {packet.IsLast}, type: {packet.PacketType}");
-
-          if (IsLastPacket(packet.PacketId, packet.IsLast))
-          {
-            pendingPackets.Remove(packetId);
-            onAllPacketsSent?.Invoke(packetId);
-          }
-        }
-        else
-        {
-          Debug.LogError("NetworkManager - WebSocket is not connected! Cannot send packets!");
-          break;
-        }
-      }
-    }
-
-    void Update()
-    {
-      // Poll the WebSocket on each frame to receive messages
-      webSocket?.DispatchMessageQueue();
-    }
-
-    private async void OnApplicationQuit()
-    {
       if (IsWebSocketOpen())
       {
-        await webSocket.Close();
+        await webSocket.SendText(packetJson);
+        onPacketSent?.Invoke(packet);
+        pendingPackets[packetId].Remove(packet);
+
+        Debug.Log($"Packet sent: {packet.PacketId}, index: {packet.Index}, isLast: {packet.IsLast}, type: {packet.PacketType}");
+
+        if (IsLastPacket(packet.PacketId, packet.IsLast))
+        {
+          pendingPackets.Remove(packetId);
+          onAllPacketsSent?.Invoke(packetId);
+        }
+      }
+      else
+      {
+        Debug.LogError("NetworkManager - WebSocket is not connected! Cannot send packets!");
+        break;
       }
     }
+  }
 
-    private bool IsWebSocketOpen()
-    {
-      return webSocket.State == WebSocketState.Open;
-    }
+  void Update()
+  {
+    // Poll the WebSocket on each frame to receive messages
+    webSocket?.DispatchMessageQueue();
+  }
 
-    private bool ArePacketsEmpty(List<Packet> packets)
+  private async void OnApplicationQuit()
+  {
+    if (IsWebSocketOpen())
     {
-      return packets.Count == 0;
+      await webSocket.Close();
     }
+  }
 
-    private bool IsLastPacket(string packetId, bool isLast)
-    {
-      return isLast && pendingPackets[packetId].Count == 0;
-    }
+  private bool IsWebSocketOpen()
+  {
+    return webSocket.State == WebSocketState.Open;
+  }
+
+  private bool ArePacketsEmpty(List<Packet> packets)
+  {
+    return packets.Count == 0;
+  }
+
+  private bool IsLastPacket(string packetId, bool isLast)
+  {
+    return isLast && pendingPackets[packetId].Count == 0;
   }
 }
