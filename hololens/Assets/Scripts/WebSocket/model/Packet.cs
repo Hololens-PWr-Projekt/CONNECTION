350126e4680ddcf5721391c780501754bf8247e3
AdrakPro <adrak99.adrak99@gmail.com>
2024-11-23 18:24:11 +0100

    Rollback to version c# 9

--- hololens/Assets/Scripts/WebSocket/model/Packet.cs
+++ hololens/Assets/Scripts/WebSocket/model/Packet.cs
@@ -2,60 +2,62 @@ using System;
 using System.ComponentModel;
 using System.Reflection;
 
-namespace Model.Packet;
-[Serializable]
-public class Packet
+namespace Model.Packet
 {
-  private string packetId;
-  private string packetType;
-  private int index;
-  private bool isLast;
-  private string chunk;
-
-  public Packet(string packetId, PacketType type, int index, bool isLast, string chunk)
+  [Serializable]
+  public class Packet
   {
-    this.PacketId = packetId;
-    this.PacketType = PacketTypeExtensions.GetDescription(type);
-    this.Index = index;
-    this.IsLast = isLast;
-    this.Chunk = chunk;
+    private string packetId;
+    private string packetType;
+    private int index;
+    private bool isLast;
+    private string chunk;
+
+    public Packet(string packetId, PacketType type, int index, bool isLast, string chunk)
+    {
+      this.PacketId = packetId;
+      this.PacketType = PacketTypeExtensions.GetDescription(type);
+      this.Index = index;
+      this.IsLast = isLast;
+      this.Chunk = chunk;
+    }
+
+    public string PacketId { get => packetId; set => packetId = value; }
+    public string PacketType { get => packetType; set => packetType = value; }
+    public int Index { get => index; set => index = value; }
+    public bool IsLast { get => isLast; set => isLast = value; }
+    public string Chunk { get => chunk; set => chunk = value; }
   }
 
-  public string PacketId { get => packetId; set => packetId = value; }
-  public string PacketType { get => packetType; set => packetType = value; }
-  public int Index { get => index; set => index = value; }
-  public bool IsLast { get => isLast; set => isLast = value; }
-  public string Chunk { get => chunk; set => chunk = value; }
-}
-
-// In future, get PacketTypes from server on startup
-public enum PacketType
-{
-  [Description("vertices")]
-  Vertices,
-  [Description("triangles")]
-  Triangles
-}
-
-// Helper class for getting enum's string description
-public static class PacketTypeExtensions
-{
-  public static string GetDescription(this Enum value)
+  // In future, get PacketTypes from server on startup
+  public enum PacketType
   {
-    Type type = value.GetType();
-    string name = Enum.GetName(type, value);
-    if (name != null)
+    [Description("vertices")]
+    Vertices,
+    [Description("triangles")]
+    Triangles
+  }
+
+  // Helper class for getting enum's string description
+  public static class PacketTypeExtensions
+  {
+    public static string GetDescription(this Enum value)
     {
-      FieldInfo field = type.GetField(name);
-      if (field != null)
+      Type type = value.GetType();
+      string name = Enum.GetName(type, value);
+      if (name != null)
       {
-        if (Attribute.GetCustomAttribute(field,
-                 typeof(DescriptionAttribute)) is DescriptionAttribute attr)
+        FieldInfo field = type.GetField(name);
+        if (field != null)
         {
-          return attr.Description;
+          if (Attribute.GetCustomAttribute(field,
+                   typeof(DescriptionAttribute)) is DescriptionAttribute attr)
+          {
+            return attr.Description;
+          }
         }
       }
+      return null;
     }
-    return null;
   }
 }
--- hololens/Assets/Scripts/WebSocket/model/Vector3D.cs
+++ hololens/Assets/Scripts/WebSocket/model/Vector3D.cs
@@ -1,22 +1,24 @@
 using System;
 
-namespace Model.Vector3D;
-
-[Serializable]
-public class Vector3D
+namespace Model.Vector3D
 {
-    private float x;
-    private float y;
-    private float z;
 
-    public Vector3D(float x, float y, float z)
+    [Serializable]
+    public class Vector3D
     {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
+        private float x;
+        private float y;
+        private float z;
 
-    public float X { get => x; set => x = value; }
-    public float Y { get => y; set => y = value; }
-    public float Z { get => z; set => z = value; }
+        public Vector3D(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
+        public float Z { get => z; set => z = value; }
+    }
 }
