d87ecc4c6b4092fbeac33689543107de0247827b
AdrakPro <adrak99.adrak99@gmail.com>
2024-11-29 10:30:54 +0100

    Refactor model

--- server/model/Vertex.cs
+++ server/model/Vertex.cs
@@ -4,15 +4,22 @@ using System;
 namespace Server.Model
 {
     [Serializable]
-    public class Vertex(float x, float y, float z)
+    public class Vertex
     {
         [JsonProperty("x")]
-        public float X { get; set; } = x;
+        public float X { get; }
 
         [JsonProperty("y")]
-        public float Y { get; set; } = y;
+        public float Y { get; }
 
         [JsonProperty("z")]
-        public float Z { get; set; } = z;
+        public float Z { get; }
+
+        public Vertex(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
     }
 }
