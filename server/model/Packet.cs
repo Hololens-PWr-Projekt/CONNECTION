6e7a590378a031ed627ae728685e51c4fdd48ff6
AdrakPro <adrak99.adrak99@gmail.com>
2024-11-29 10:30:54 +0100

    Refactor model

--- server/model/Packet.cs
+++ server/model/Packet.cs
@@ -1,96 +1,79 @@
 using System;
-using System.ComponentModel;
-using System.Reflection;
+using Newtonsoft.Json;
 
 namespace Server.Model
 {
-  [Serializable]
-  public class Packet
-  {
-    private string packetId;
-    private string packetType;
-    private Chunk chunk;
-    private Metadata metadata;
-
-    public Packet(string packetId, PacketType type, Chunk chunk, Metadata metadata)
+    [Serializable]
+    public class Packet
     {
-      this.PacketId = packetId;
-      this.PacketType = PacketTypeExtensions.GetDescription(type);
-      this.Chunk = chunk;
-      this.Metadata = metadata;
-    }
+        [JsonProperty("packetId")]
+        public string PacketId { get; }
 
-    public string PacketId { get => packetId; set => packetId = value; }
-    public string PacketType { get => packetType; set => packetType = value; }
-    public Chunk Chunk { get => chunk; set => chunk = value; }
-    public Metadata Metadata { get => metadata; set => metadata = value; }
-    public bool IsLast()
-    {
-      return chunk.SequenceNumber == chunk.TotalChunks;
-    }
-  }
+        [JsonProperty("packetType")]
+        public string PacketType { get; }
 
-  [Serializable]
-  public class Chunk
-  {
-    private int sequenceNumber;
-    private int totalChunks;
-    private string data;
+        [JsonProperty("chunk")]
+        public Chunk Chunk { get; }
 
-    public Chunk(int sequenceNumber, int totalChunks, string data)
-    {
-      this.SequenceNumber = sequenceNumber;
-      this.TotalChunks = totalChunks;
-      this.Data = data;
-    }
+        [JsonProperty("metadata")]
+        public Metadata Metadata { get; }
 
-    public int SequenceNumber { get => sequenceNumber; set => sequenceNumber = value; }
-    public int TotalChunks { get => totalChunks; set => totalChunks = value; }
-    public string Data { get => data; set => data = value; }
-  }
-
-  [Serializable]
-  public class Metadata
-  {
-    private int chunkSizeBytes;
-
-    public Metadata(int chunkSizeBytes)
-    {
-      this.ChunkSizeBytes = chunkSizeBytes;
-    }
-
-    public int ChunkSizeBytes { get => chunkSizeBytes; set => chunkSizeBytes = value; }
-  }
-
-  // In future, get PacketTypes from server on startup
-  public enum PacketType
-  {
-    [Description("vertices")]
-    Vertices,
-    [Description("triangles")]
-    Triangles
-  }
-
-  // Helper class for getting enum's string description
-  public static class PacketTypeExtensions
-  {
-    public static string GetDescription(this Enum value)
-    {
-      Type type = value.GetType();
-      string name = Enum.GetName(type, value);
-      if (name != null)
-      {
-        FieldInfo field = type.GetField(name);
-        if (field != null)
+        public Packet(string packetId, string packetType, Chunk chunk, Metadata metadata)
         {
-          if (Attribute.GetCustomAttribute(field,
-                   typeof(DescriptionAttribute)) is DescriptionAttribute attr)
-          {
-            return attr.Description;
-          }
+            PacketId = packetId;
+            PacketType = packetType;
+            Chunk = chunk;
+            Metadata = metadata;
         }
-      }
-      return null;
     }
-  }
+
+    [Serializable]
+    public class Chunk
+    {
+        [JsonProperty("sequenceNumber")]
+        public int SequenceNumber { get; }
+
+        [JsonProperty("totalChunks")]
+        public int TotalChunks { get; }
+
+        [JsonProperty("data")]
+        public string Data { get; }
+
+        public Chunk(int sequenceNumber, int totalChunks, string data)
+        {
+            if (totalChunks <= 0)
+                throw new ArgumentException(
+                    "Total chunks must be greater than 0.",
+                    nameof(totalChunks)
+                );
+
+            if (sequenceNumber < 1 || sequenceNumber > totalChunks)
+                throw new ArgumentOutOfRangeException(
+                    nameof(sequenceNumber),
+                    "Sequence number must be within the range of total chunks."
+                );
+
+            SequenceNumber = sequenceNumber;
+            TotalChunks = totalChunks;
+            Data = data;
+        }
+    }
+
+    [Serializable]
+    public class Metadata
+    {
+        [JsonProperty("chunkBytes")]
+        public int ChunkBytes { get; }
+
+        public Metadata(int chunkBytes)
+        {
+            if (chunkBytes <= 0)
+                throw new ArgumentException(
+                    "Chunk size must be greater than 0.",
+                    nameof(chunkBytes)
+                );
+
+            ChunkBytes = chunkBytes;
+        }
+    }
 }
