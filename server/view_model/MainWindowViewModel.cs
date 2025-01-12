c10eb50062fe6b2225a6a52bf77140956bb6b194
AdrakPro <adrak99.adrak99@gmail.com>
2025-01-12 23:36:03 +0100

    Added hiding data in view

--- server/view_model/MainWindowViewModel.cs
+++ server/view_model/MainWindowViewModel.cs
@@ -1,15 +1,15 @@
+using ReactiveUI;
+using Server.Model;
+using Server.Service;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Avalonia.Threading;
-using Newtonsoft.Json.Linq;
-using ReactiveUI;
-using Server.Model;
-using Server.Service;
+using System.Threading.Tasks;
+using System.Reactive;
 
 namespace Server.ViewModel
 {
-    // todo reactive ui causes lag because of so much data send
     public class MainWindowViewModel : ReactiveObject
     {
         private readonly WebSocketManager webSocketManager;
@@ -17,8 +17,10 @@ namespace Server.ViewModel
 
         private List<Vertex> vertices;
         private List<int> triangles;
+        private bool hideData;
 
         public ObservableCollection<string> Messages { get; } = [];
+
         public List<Vertex> Vertices
         {
             get => vertices;
@@ -31,10 +33,20 @@ namespace Server.ViewModel
             set => this.RaiseAndSetIfChanged(ref triangles, value);
         }
 
+        public bool HideData
+        {
+            get => hideData;
+            set => this.RaiseAndSetIfChanged(ref hideData, value);
+        }
+
+        public string HideDataStatus => $"Hide Data: {HideData}";
+
         public MainWindowViewModel()
         {
             vertices = [];
             triangles = [];
+
+            hideData = true;
 
             dataProcessor = new DataProcessor(this);
             webSocketManager = new WebSocketManager(OnMessageReceived, OnMessageSent, OnError);
@@ -72,25 +84,12 @@ namespace Server.ViewModel
 
         private void AppendMessage(string message)
         {
-            // Use Dispatcher to ensure UI thread safety
             Dispatcher.UIThread.InvokeAsync(() => Messages.Add(message));
-        }
-
-        private static bool IsValidJson(string input)
-        {
-            try
-            {
-                var obj = JToken.Parse(input);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
         }
 
         private void OnMessageSent(string message) => AppendMessage($"\nSent: {message}");
 
         private void OnError(Exception e) => AppendMessage($"\nError: {e.Message}");
+
     }
 }
