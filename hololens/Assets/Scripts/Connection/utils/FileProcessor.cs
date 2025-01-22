54b068d95fea7178e006fa1bbe808fa38b4c24e0
AdrakPro <adrak99.adrak99@gmail.com>
2025-01-22 16:01:26 +0100

    UWP compatbile

--- hololens/Assets/Scripts/Connection/utils/FileProcessor.cs
+++ hololens/Assets/Scripts/Connection/utils/FileProcessor.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using Hololens.Assets.Scripts.Connection.Model;
 using UnityEngine;
+#if WINDOWS_UWP
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+#endif
 
 namespace Hololens.Assets.Scripts.Connection.Utils
 {
@@ -10,13 +15,34 @@ namespace Hololens.Assets.Scripts.Connection.Utils
     {
         public static byte[] ReadFile(string filePath)
         {
-            if (!File.Exists(filePath))
+#if WINDOWS_UWP
+            return ReadFileUWP(filePath).Result;
+#else
+            if (!System.IO.File.Exists(filePath))
             {
-                throw new FileNotFoundException($"File was not found: {filePath}");
+                throw new System.IO.FileNotFoundException($"File was not found: {filePath}");
             }
 
-            return File.ReadAllBytes(filePath);
+            return System.IO.File.ReadAllBytes(filePath);
+#endif
         }
+
+#if WINDOWS_UWP
+        private static async Task<byte[]> ReadFileUWP(string filePath)
+        {
+            StorageFile file = await StorageFile.GetFileFromPathAsync(filePath);
+            using (IRandomAccessStream stream = await file.OpenReadAsync())
+            {
+                byte[] data = new byte[stream.Size];
+                using (DataReader reader = new DataReader(stream))
+                {
+                    await reader.LoadAsync((uint)stream.Size);
+                    reader.ReadBytes(data);
+                }
+                return data;
+            }
+        }
+#endif
 
         public static IEnumerable<Packet> SplitFile(string channelName, byte[] data)
         {
@@ -61,14 +87,17 @@ namespace Hololens.Assets.Scripts.Connection.Utils
 
         public static void ReassembleFile(string channelName, List<Packet> packets)
         {
-            string outputPath = Path.Combine(
+#if WINDOWS_UWP
+            ReassembleFileUWP(channelName, packets).Wait();
+#else
+            string outputPath = System.IO.Path.Combine(
                 Environment.CurrentDirectory,
                 $"{channelName}_{DateTime.UtcNow:yyyyMMddHHmmss}.obj"
             );
 
             packets.Sort((a, b) => a.Chunk.SequenceNumber.CompareTo(b.Chunk.SequenceNumber));
 
-            using (var fileStream = new FileStream(outputPath, FileMode.Create))
+            using (var fileStream = new System.IO.FileStream(outputPath, System.IO.FileMode.Create))
             {
                 foreach (var packet in packets)
                 {
@@ -77,6 +106,39 @@ namespace Hololens.Assets.Scripts.Connection.Utils
             }
 
             Debug.Log($"Reassembled file saved at: {outputPath}");
+#endif
         }
+
+#if WINDOWS_UWP
+        private static async Task ReassembleFileUWP(string channelName, List<Packet> packets)
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            string fileName = $"{channelName}_{DateTime.UtcNow:yyyyMMddHHmmss}.obj";
+            StorageFile outputFile = await storageFolder.CreateFileAsync(
+                fileName,
+                CreationCollisionOption.ReplaceExisting
+            );
+
+            packets.Sort((a, b) => a.Chunk.SequenceNumber.CompareTo(b.Chunk.SequenceNumber));
+
+            using (
+                IRandomAccessStream stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite)
+            )
+            {
+                using (DataWriter writer = new DataWriter(stream))
+                {
+                    foreach (var packet in packets)
+                    {
+                        writer.WriteBytes(packet.Chunk.Data);
+                    }
+
+                    await writer.StoreAsync();
+                    await writer.FlushAsync();
+                }
+            }
+
+            Debug.Log($"Reassembled file saved at: {outputFile.Path}");
+        }
+#endif
     }
 }
